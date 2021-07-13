ece0ba4118184c55c1bd9263f5bc33bd4339d7a5
LinkGian <59111263+LinkGian@users.noreply.github.com>
2021-07-12 19:06:46 -0600

    smartContract
    price/volume feed
    VRF

---
+++ myFirstContract.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.6.7;
+
+contract MyFirstContract {
+ 
+    uint256 number = 0;
+    number = number + _num;
+    
+    function getNumberMultiplied(_num) public view returns (unit256) {
+        number = _num1 * number;
+    }
+    
+    function addNumbers(_num1, _num2) public view returns (unit256);
+        number = _num1 + _num2;
+ 
+ 
+    function changeNumber(uint256 _num) public {
+        number = _num;
+    }
+ 
+ 
+    function getNumber() public view returns (uint256){
+        return number;
+    }
+}
